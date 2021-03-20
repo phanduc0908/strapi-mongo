c705dd039b274f7176c9f5bfe3b589703e4705f7
phanduc0908 <phanduc0908@gmail.com>
2021-03-20 10:05:34 +0700

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ee52503252e0064982a1b9ea51e873e0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f1cb6b43-8a38-4fb6-a48d-c63639ca4a48'
+};
